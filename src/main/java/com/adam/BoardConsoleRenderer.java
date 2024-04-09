b8e49b59b6290e6615dd22d90937320fe0efa2bf
Adam <66186445+AdamabuMedina@users.noreply.github.com>
2024-04-10 00:26:43 +0200

    донастройка метода setupDefaultPiecesPositions

--- src/main/java/com/adam/BoardConsoleRenderer.java
+++ src/main/java/com/adam/BoardConsoleRenderer.java
@@ -8,22 +8,22 @@ public class BoardConsoleRenderer {
     public static final String ANSI_BLACK_PIECE_COLOR = "\u001B[30m";
     public static final String ANSI_WHITE_SQUARE_BACKGROUND = "\u001B[47m";
     public static final String ANSI_BLACK_SQUARE_BACKGROUND = "\u001B[0;100m";
+    public static final String ANSI_HIGHLIGHTED_SQUARE_BACKGROUND = "\u001B[45m";
 
 
     public void render(Board board) {
         for (int rank = 8; rank >= 1; rank--) {
             StringBuilder line = new StringBuilder();
+            ;
             for (File file : File.values()) {
                 Coordinates coordinates = new Coordinates(file, rank);
-                if(board.isSquareEmpty(coordinates)) {
+                if (board.isSquareEmpty(coordinates)) {
                     line.append(getSpringForEmptySquare(coordinates));
                 } else {
                     line.append(getPieceSprite(board.getPiece(coordinates)));
                 }
             }
-
             line.append(ANSI_RESET);
-
             System.out.println(line);
         }
     }
@@ -35,7 +35,7 @@ public class BoardConsoleRenderer {
         if (pieceColor == Color.WHITE) {
             result = ANSI_WHITE_PIECE_COLOR + result;
         } else {
-            result = ANSI_BLACK_PIECE_COLOR;
+            result = ANSI_BLACK_PIECE_COLOR + result;
         }
 
         if (isSquareDark) {
@@ -52,6 +52,22 @@ public class BoardConsoleRenderer {
     }
 
     private String getPieceSprite(Piece piece) {
-        return colorizeSprite(" A ", piece.color, Board.isSquareDark(piece.coordinates));
+        return colorizeSprite(
+                " " + selectUnicodeSpriteForPiece(piece) + " ",
+                piece.color,
+                Board.isSquareDark(piece.coordinates));
+    }
+
+    private String selectUnicodeSpriteForPiece(Piece piece) {
+        return switch (piece.getClass().getSimpleName()) {
+            case "Pawn" -> "♟";
+            case "Knight" -> "♞";
+            case "Bishop" -> "♝";
+            case "Rook" -> "♜";
+            case "Queen" -> "♛";
+            case "King" -> "♚";
+            default -> "";
+        };
+
     }
 }
