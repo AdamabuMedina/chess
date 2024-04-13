d2334e07ef3fac834fa8324c3eca8df91315dd8c
Adam <66186445+AdamabuMedina@users.noreply.github.com>
2024-04-13 08:14:26 +0200

    реализация метода getPieceMoves для Bishop

--- src/main/java/com/adam/piece/Bishop.java
+++ src/main/java/com/adam/piece/Bishop.java
@@ -3,6 +3,7 @@ package main.java.com.adam.piece;
 import main.java.com.adam.Color;
 import main.java.com.adam.Coordinates;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Bishop extends Piece {
@@ -12,6 +13,22 @@ public class Bishop extends Piece {
 
     @Override
     protected Set<CoordinatesShift> getPieceMoves() {
-        return Set.of();
+        Set<CoordinatesShift> result = new HashSet<>();
+
+        // bottom-left to bottom-right
+        for (int i = -7; i < 7; i++) {
+            if (i == 0) continue;
+
+            result.add(new CoordinatesShift(i, i));
+        }
+
+        // top-left to bottom-right
+        for (int i = -7; i < 7; i++) {
+            if (i == 0) continue;
+
+            result.add(new CoordinatesShift(i, -i));
+        }
+
+        return result;
     }
 }
