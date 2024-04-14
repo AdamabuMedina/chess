5fa0d940cf87e4f2380ae1311ddee6320764faf4
Adam <66186445+AdamabuMedina@users.noreply.github.com>
2024-04-14 05:35:44 +0200

    реализация ходов пешки Pawn

--- src/main/java/com/adam/Main.java
+++ src/main/java/com/adam/Main.java
@@ -2,26 +2,24 @@ package main.java.com.adam;
 
 public class Main {
     public static void main(String[] args) {
-//        Board board = new Board();
-//        board.setupDefaultPiecesPositions();
+        // Board board = new Board();
+        // board.setupDefaultPiecesPositions();
 
         Board board = (new BoardFactory()).fromFEN(
-//                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-//                 "3k4/8/5n2/2N5/3B4/8/8/3K4 w - - 0 1"
-//                "3k4/8/p7/8/R7/8/P7/3K4 w - - 0 1"
-//                "3k4/8/p5p1/5B2/R7/3P4/P7/3K4 w - - 0 1"
-                "3p4/6b1/8/1P2Q3/2B5/6P1/2R1r3/3P4 w - - 0 1"
+                "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+        // "3k4/8/5n2/2N5/3B4/8/8/3K4 w - - 0 1"
+        // "3k4/8/p7/8/R7/8/P7/3K4 w - - 0 1"
+        // "3k4/8/p5p1/5B2/R7/3P4/P7/3K4 w - - 0 1"
+        // "3p4/6b1/8/1P2Q3/2B5/6P1/2R1r3/3P4 w - - 0 1"
         );
 
+        // BoardConsoleRenderer renderer = new BoardConsoleRenderer();
+        // renderer.render(board);
 
-        BoardConsoleRenderer renderer = new BoardConsoleRenderer();
-//        renderer.render(board);
-
-
-//        Piece piece = board.getPiece(new Coordinates(File.B, 1));
-//        Set<Coordinates> availableMoveSquare = piece.getAvailableMoveSquare(board);
-//
-//        int a = 123;
+        // Piece piece = board.getPiece(new Coordinates(File.B, 1));
+        // Set<Coordinates> availableMoveSquare = piece.getAvailableMoveSquare(board);
+        //
+        // int a = 123;
 
         Game game = new Game(board);
         game.gameLoop();
--- src/main/java/com/adam/piece/Pawn.java
+++ src/main/java/com/adam/piece/Pawn.java
@@ -3,6 +3,7 @@ package main.java.com.adam.piece;
 import main.java.com.adam.Color;
 import main.java.com.adam.Coordinates;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Pawn extends Piece {
@@ -12,6 +13,22 @@ public class Pawn extends Piece {
 
     @Override
     protected Set<CoordinatesShift> getPieceMoves() {
-        return Set.of();
+        Set<CoordinatesShift> result = new HashSet<>();
+
+        if (color == Color.WHITE) {
+            result.add(new CoordinatesShift(0, 1));
+
+            if (coordinates.rank == 2) {
+                result.add(new CoordinatesShift(0, 2));
+            }
+        } else {
+            result.add(new CoordinatesShift(0, -1));
+
+            if (coordinates.rank == 7) {
+                result.add(new CoordinatesShift(0, -2));
+            }
+        }
+
+        return result;
     }
 }
